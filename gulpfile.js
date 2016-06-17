69c9ed8de73e78e53154b5c35fe1056cefa8178d
Jesada <jesada.mtk@gmail.com>
2016-06-17 14:13:39 +0700

    update portfolio

--- gulpfile.js
+++ gulpfile.js
@@ -12,5 +12,9 @@ var elixir = require('laravel-elixir');
  */
 
 elixir(function(mix) {
-    mix.sass('app.scss');
+  mix.scripts('*');
+  mix.sass('app.scss');
+  // mix.browserSync({
+  //   proxy: 'polardevs.app'
+  // });
 });
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -19,6 +19,11 @@
                 font-family: 'Lato';
             }
 
+            img {
+                height: 300px;
+                width: auto;
+            }
+
             .container {
                 text-align: center;
                 display: table-cell;
@@ -38,9 +43,22 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Yeah I got it!!</div>
-                <img src="https://media.giphy.com/media/fSHgyXYXy8Sd2/giphy.gif">
-                <div>Deploy by rocketeer.</div>
+                <div class="title"><strong> ~ Polardevs's coming soon ~ </strong></div>
+                <div><strong>## IF U WANT A WEBSITE PLEASE CONTACT ME ##</strong></div>
+                <div><strong>YOU CAN SEE MY SIDE PROJECT HERE :</strong></div>
+                <div>
+                    <a href="http://gmt.polardevs.com"><input type="button" value="Timesheet"></a>
+                    <a href="http://nurse.polardevs.com"><input type="button" value="Web Content"></a>
+                    <a href="http://issara.polardevs.com"><input type="button" value="Web Content"></a>
+                </div>
+                <div>NAME       : JESADA MAYTHANGKUL - (OHM)</div>
+                <div>TEL        : 093-324-7915</div>
+                <div>EMAIL      : JESADA@POLARDEVS.COM</div>
+                <div>LINE       : OHMMTK</div>
+                <div>PRICE      : 15,000 ++</div>
+                <div>EXPERIENCE : PHP, LARAVEL, BOOTSTRAP, HTML5, JAVASCRIPT, JQUERY</div>
+                <!-- <div><img src="https://media.giphy.com/media/fSHgyXYXy8Sd2/giphy.gif"></div> -->
+                <div><img src="https://media.giphy.com/media/4t9P4d6pqjtja/giphy.gif"></div>
             </div>
         </div>
     </body>
